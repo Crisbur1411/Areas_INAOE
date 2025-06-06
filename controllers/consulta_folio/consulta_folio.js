1bf2e1fc96a7691aead0ebb4108e8e83ef51559c
Cristian <crisbur141102@gmail.com>
2025-06-06 12:19:11 -0600

    Implementación de busqueda de folios para obtener constancias de liberacion

---
+++ controllers/consulta_folio/consulta_folio.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('#validationForm').on('submit', function (e) {
+        e.preventDefault();
+
+        const folio = $('#folio').val().trim();
+
+        if (folio === "") {
+            alert("Por favor ingresa un folio.");
+            return;
+        }
+
+        const pdfUrl = `../../res/temp/${folio}.pdf`;
+
+        // obtener el archivo
+        fetch(pdfUrl, { method: 'HEAD' })
+            .then(response => {
+                if (response.ok) {
+                    // Mostrar alerta de éxito
+                    $('#alert_success').show();
+                    $('#alert_error').hide();
+
+                    // Abrir PDF en nueva pestaña
+                    window.open(pdfUrl, '_blank');
+
+                    $('#folio').val('');
+                } else {
+                    // Mostrar alerta de error
+                    $('#alert_error').show();
+                    $('#alert_success').hide();
+                }
+            })
+            .catch(error => {
+                console.error('Error al buscar la constancia:', error);
+                $('#alert_error').show();
+                $('#alert_success').hide();
+            });
+    });
+});
